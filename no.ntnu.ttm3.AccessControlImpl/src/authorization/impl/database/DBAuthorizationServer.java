44fc61adf157e62394fc12096c819968163f4c64
yorn <git@yorn.priv.no>
2013-10-30 16:48:58 +0100

    Cleaned imports

--- no.ntnu.ttm3.AccessControlImpl/src/authorization/impl/database/DBAuthorizationServer.java
+++ no.ntnu.ttm3.AccessControlImpl/src/authorization/impl/database/DBAuthorizationServer.java
@@ -1,10 +1,5 @@
 package authorization.impl.database;
 
-import hydna.ntnu.student.api.HydnaApi;
-import aQute.bnd.annotation.component.Activate;
-import aQute.bnd.annotation.component.Reference;
-import authorization.api.IAuthorizationServer;
-import authorization.impl.AuthorizationServer;
 
 public class DBAuthorizationServer {
 
--- no.ntnu.ttm3.AccessControlImpl/src/authorization/impl/timedauth/TimedAuthorizationServer.java
+++ no.ntnu.ttm3.AccessControlImpl/src/authorization/impl/timedauth/TimedAuthorizationServer.java
@@ -1,11 +1,6 @@
 package authorization.impl.timedauth;
 
-import hydna.ntnu.student.api.HydnaApi;
-import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
-import aQute.bnd.annotation.component.Reference;
-import authorization.api.IAuthorizationServer;
-import authorization.impl.AuthorizationServer;
 
 @Component
 public class TimedAuthorizationServer {
